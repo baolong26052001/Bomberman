561a66c4032c5aed33494e459022e51fd8aeba00
khangng2001 <72006373+khangng2001@users.noreply.github.com>
2022-12-09 15:38:05 +0700

    basic gameOver

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,33 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] players;
 
+    public GameObject gameOver;
+    public TMP_Text playerWon;
+
     public void CheckWinState()
     {
         int aliveCount = 0;
 
+
         foreach (GameObject player in players)
         {
-            if(player.activeSelf)
+            if (player.activeSelf)
             {
                 aliveCount++;
+                gameOver.SetActive(true);
+                playerWon.text = player.name + " won";
+                
             }
         }
-        if(aliveCount <= 1)
+        if (aliveCount <= 1)
         {
-            Invoke(nameof(NewRound),1f);
+            Invoke(nameof(NewRound), 1f);
         }
-        
-        
 
-
+        
     }
     private void NewRound()
     {
-        SceneManager.LoadScene("GameOverScreen");
+        //SceneManager.LoadScene("GameOverScreen");
     }
 }
